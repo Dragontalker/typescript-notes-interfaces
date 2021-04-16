395122ebfeb59ceb58257dcc010d6eccbb18e003
Richard <richard.yang.tong@gmail.com>
2021-04-16 16:16:46 -0400

    Added another diffrent type of object.

--- interfaces.ts
+++ interfaces.ts
@@ -11,6 +11,15 @@ const oldCivic = {
   }
 };
 
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
+};
+
 const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
