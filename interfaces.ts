2d9df040c670340d74223dc3c24d85a1d8db90d5
Richard <richard.yang.tong@gmail.com>
2021-04-16 15:52:32 -0400

    Added type for methods.

--- interfaces.ts
+++ interfaces.ts
@@ -1,17 +1,21 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic: Vehicle = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`
+  }
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
+  console.log(vehicle.summary);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken? ${vehicle.broken}`);
 };
