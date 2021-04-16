29e1bd74bcc66e91ddd30bdb373bf78fa2042eb1
Richard <richard.yang.tong@gmail.com>
2021-04-16 15:43:28 -0400

    Added a typed functin.

--- interfaces.ts
+++ interfaces.ts
@@ -1,6 +1,11 @@
 const oldCivic = {
   name: 'civic',
   year: 2000,
-  borken: true
+  broken: true
 };
 
+const printVehicle = (vehicle: { name: string; year: number; broken: boolean}): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken? ${vehicle.broken}`);
+};
